53b517a512faf34a80a36b3436124cca379dfa9d
Sameeh <sameehkhan1@gmail.com>
2019-02-27 21:33:12 -0800

    i

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 
-document.addEventListener('DOMContentLoaded', () => {
-    const root = document.getElementById('root');
-    ReactDOM.render(<h1>Weather App</h1>, root);
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById("root");
+    let preloadedState = {}
+    const store = configureStore(preloadedState);
+    window.store = store;
+    ReactDOM.render(<Root store={store} />, root);
 });
