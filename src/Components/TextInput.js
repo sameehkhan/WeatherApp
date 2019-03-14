56bfea97b42b3f7265ad1301a2b267bbb741ddfc
Sameeh <sameehkhan1@gmail.com>
2019-03-13 21:24:33 -0700

    Create Text Field component class

--- src/Components/TextInput.js
+++ src/Components/TextInput.js
@@ -1,25 +1,26 @@
 import React from "react";
 import TextField from "../../node_modules/@material-ui/core/TextField";
 
+class TextInput extends React.Component {
 
-
-const TextInput = () => {
-  return (
-    <div
-      style={{
-        height: "50vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center"
-      }}
-    >
-      <TextField
-        fullWidth
-        label="Weather"
-        style={{ marginLeft: "10%", marginRight: "10%" }}
-      />
-    </div>
-  );
-};
+  render() {
+    return (
+      <div
+        style={{
+          height: "50vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        <TextField
+          fullWidth
+          label="Weather"
+          style={{ marginLeft: "10%", marginRight: "10%" }}
+        />
+      </div>
+    )
+  }
+}
 
 export default TextInput;
